4316af72c04a2d0b08c81e102322f58fd233cfcb
Julio Lopez <julioj.lopeza@gmail.com>
2021-03-16 07:59:43 +0100

    Removed item when deleted instead of hidding it

--- js/script.js
+++ js/script.js
@@ -7,19 +7,34 @@
         const newElement = $(`<li>${value}</li>`)
         const deleteButton = $('<crossOutButton>X</crossOutButton>')
         newElement.append(deleteButton)
-
-        if(value){
-            list.append(newElement)
-
-            // event listener to cross out item on double click
-            newElement.on('dblclick', () => crossOut(newElement) )
-
-            //event listener to delete items on click
-            deleteButton.on('click', () => deleteListItem(newElement) )
+        if(!isListed(newElement.text())){
+            if(value){
+                list.append(newElement)
+    
+                // event listener to cross out item on double click
+                newElement.on('dblclick', () => crossOut(newElement) )
+    
+                //event listener to delete items on click
+                deleteButton.on('click', () => deleteListItem(newElement) )
+            }else{
+                alert("You must write something")
+            }
         }else{
-            alert("You must write something")
+            alert("This is an existing TODO, try with a different name")
         }
     })
+
+    // Function to check if a todo already exist
+    function isListed(value){
+        let listed = false
+        $('li').each(function(){
+            if($(this).text() === value){
+                listed = true
+                return false
+            }
+        })
+        return listed
+    }
 
     // function to cross out items
     function crossOut(element){
@@ -28,7 +43,7 @@
 
     // function to delete items
     function deleteListItem(element){
-        element.addClass('delete')
+        element.remove()
     }
 
     // Function to make the list s
